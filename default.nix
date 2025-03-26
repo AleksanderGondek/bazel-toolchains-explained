f715ab55742a8d7fcfdc5ed65e92157f7843d509
Aleksander Gondek <gondekaleksander@protonmail.com>
2025-03-26 16:42:21 +0000

    chore(bazel): move user_root to dedicated dir

--- default.nix
+++ default.nix
@@ -8,34 +8,37 @@
   nixpkgs = import external_sources.nixpkgs nixpkgs_import_args;
 
   # To use bazelisk, not Bazel
-  devShell = (nixpkgs.buildFHSEnv {
-    name = "bazel_toolchains_explained-shell";
+  devShell =
+    (nixpkgs.buildFHSEnv {
+      name = "bazel_toolchains_explained-shell";
 
-    targetPkgs = pkgs: (with pkgs; [
-      alejandra
-      bashInteractive
-      bazelisk
-      bazel-buildtools
-      cocogitto
-      git
-      helix
-      niv
-      statix
-      # Required by downloaded bazel
-      zlib
-    ]);
+      targetPkgs = pkgs: (with pkgs; [
+        alejandra
+        bashInteractive
+        bazelisk
+        bazel-buildtools
+        cocogitto
+        git
+        helix
+        niv
+        statix
+        # Required by downloaded bazel
+        zlib
+      ]);
 
-    runScript = nixpkgs.writeScript "bazel_toolchains_explained-shell-init.sh" ''
-      cat <<EOF > user.bazelrc
-      # Bazel from nix fails processing of repo-rules
-      # without this being set (sic!)
-      # TODO: Investigate further.
-      # (/bin:/usr/bin are for the benefit of the 'hermetic' gcc toolchain...)
-      common --action_env='PATH=/bin:/usr/bin:$PATH'
-      EOF
-      exec bash
-    '';
-  }).env;
+      runScript = nixpkgs.writeScript "bazel_toolchains_explained-shell-init.sh" ''
+        cat <<EOF > user.bazelrc
+        # Bazel from nix fails processing of repo-rules
+        # without this being set (sic!)
+        # TODO: Investigate further.
+        # (/bin:/usr/bin are for the benefit of the 'hermetic' gcc toolchain...)
+        common --action_env='PATH=/bin:/usr/bin:$PATH'
+        startup --output_user_root=$PWD/bazel-user-root/
+        EOF
+        exec bash
+      '';
+    })
+    .env;
 in {
   inherit devShell nixpkgs;
 }
